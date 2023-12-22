0edc5c0f1c2a88ae697b31a11cfe321368d1c1f5
Daniel Petersen <danielpetersen@superahost.com>
2023-12-22 11:59:20 -0400

    feat: view show

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -16,6 +16,15 @@ class SupportController extends Controller
         return view('admin/supports/index', compact('supports'));
     }
 
+    public function show(string|int $support)
+    {
+        if (!$support = Support::find($support)) {
+            return back();
+        }
+
+        return view('admin.supports.show', compact('support'));
+    }
+
     public function create()
     {
         return view('admin/supports/create');
